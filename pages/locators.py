60a341482a8344d14d5bae5e09cad3564fde2074
iim666 <majesty696@gmail.com>
2020-08-30 15:57:24 +0200

    Updated and added tests as required in Module 4, Submodule 4.3, Step 8

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators():
     BOOK_NAME_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
     BOOK_PRICE_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(3) strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.product_page import ProductPage
 
 book_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -25,7 +25,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_book_name()
     product_page.should_be_book_price()
 
-
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, book_link)
@@ -34,13 +34,13 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message()
 
-
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     product_page = ProductPage(browser, book_link)
     product_page.open()
     product_page.should_not_be_success_message()
 
-
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, book_link)
@@ -48,3 +48,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_disappead()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
