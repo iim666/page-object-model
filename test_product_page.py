078c985f66a35f7aa46f00b027a5297ca75b80e7
iim666 <majesty696@gmail.com>
2020-08-30 18:39:40 +0200

    Last commit for Code Review. Module 4, Submodule 4.3, Step 14

--- test_product_page.py
+++ test_product_page.py
@@ -25,6 +25,7 @@ class TestUserAddToBasketFromProductPage:
         product_page.open()
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         product_page = ProductPage(browser, book_link)
         product_page.open()
@@ -32,8 +33,15 @@ class TestUserAddToBasketFromProductPage:
         product_page.should_be_book_name()
         product_page.should_be_book_price()
 
+@pytest.mark.need_review
+def test_quest_can_add_product_to_basket(browser):
+    product_page = ProductPage(browser, book_link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_be_book_name()
+    product_page.should_be_book_price()
 
-@pytest.mark.skip
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, book_link)
@@ -43,7 +51,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message()
 
 
-@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, book_link)
@@ -53,22 +60,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.should_be_disappead()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, book_link)
     page.open()
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, book_link)
     page.open()
     page.go_to_login_page()
 
 
-@pytest.mark.skip
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, book_link)
     page.open()
     page.go_to_basket()
